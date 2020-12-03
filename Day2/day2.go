9743726fd9ef53d4f5a1e143eba3a307e7dd0936
Karl Schwirz <schwirz.karl@bcg.com>
2020-12-03 00:27:12 -0500

    Cleaning up code

--- Day2/day2.go
+++ Day2/day2.go
@@ -26,7 +26,8 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		if(pwChecker(scanner.Text())){
+		// if(pwChecker(scanner.Text())){
+		if(dumbassShopKeeperChecker(scanner.Text())){
 			pass++
 		} else {
 			fail++
@@ -83,4 +84,50 @@ func pwChecker(entry string) bool {
 	// Pass/Fail | letter | count | range | password
 	fmt.Println(state, s, l, s, fmt.Sprintf("%2v", ct), s, fmt.Sprintf("%5v", c), s, fmt.Sprintf("%20v", pw), s)
 	return v
+}
+
+func dumbassShopKeeperChecker(entry string) bool {
+	
+	//Get the range to validate against
+	b := getPolicy(entry, 0)
+	e := getPolicy(entry, 1)
+
+	n := strings.Split(entry, " ")
+
+	// Get the value of the letter validator
+	l := strings.Replace(n[1], ":", "", -1)
+	
+	// Get the PW
+	pw := n[2]
+
+	// Validate by position
+	v := false
+	aEntry := []rune(pw)
+	check1 := string(aEntry[b-1]) == l
+	check2 := string(aEntry[e-1]) == l
+
+	if(check1 || check2) {
+		if(check1 && check2){
+			v = false
+		} else {
+			v = true
+		}
+	}
+
+	// Determine if password is valid
+	state := fmt.Sprintf("%6v", "Failed")
+	if(v) {
+		state = fmt.Sprintf("%6v", "Valid")
+	} 
+	s := " | "
+	// Pass/Fail | letter | count | range | password
+	fmt.Println(state, s, fmt.Sprintf("%5v", check1), s, fmt.Sprintf("%5v", check2), s, l, s, fmt.Sprintf("%20v", pw), s)
+	return v
+}
+
+func getPolicy (entry string, position int) int {
+	r, _ := regexp.Compile("\\d+\\-\\d+")
+	c := r.FindString(entry)	
+	n, _ := strconv.Atoi(strings.Split(c, "-")[position])
+	return n
 }
