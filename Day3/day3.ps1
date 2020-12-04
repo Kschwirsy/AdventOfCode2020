27e55f2da4356a83b76f443068f6830732cf1731
Karl Schwirz <schwirz.karl@bcg.com>
2020-12-04 00:33:23 -0500

    refactored to function to solve first and second problem

--- Day3/day3.ps1
+++ Day3/day3.ps1
@@ -1,32 +1,41 @@
-$y = 0
-$map=@{}
+function HappyTrails {
+    param (
+        $right, 
+        $down
+    )
 
-# Build Tree Map
-foreach($line in Get-Content AdventOfCode2020\Day3\input.txt) {
-    $map[$y] = $line.ToCharArray()
-    $y++
-}
+    $y = 0
+    $map=@{}
 
-# Get height of hill and width of the map for rollover
-$xMax = $map[0].Count
-$yMax = $map.Count-1
-
-# Set up to ride
-$hits = 0
-$lat = 0
-
-# Down we go
-for ($long = 0; $long -le $yMax; $long++) {
-    if($map[$long][$lat % $xMax] -eq '#') {
-        $map[$long][$lat % $xMax] = 'X'
-        $hits++
-    } else {
-        $map[$long][$lat % $xMax] = 'O'
+    # Build Tree Map
+    foreach($line in Get-Content input.txt) {
+        $map[$y] = $line.ToCharArray()
+        $y++
     }
 
-    # Shift 3 over for the next line
-    $lat+=3
-    Write-Host("{2} | {1} | {0}" -f ($long+1), ($map[$long]-join ""), $hits)
+    # Get height of hill and width of the map for rollover
+    $xMax = $map[0].Count
+    $yMax = $map.Count-1
+
+    # Set up to ride
+    $hits = 0
+    $lat = 0
+
+    # Down we go
+    for ($long = 0; $long -le $yMax; $long+=$down) {
+        if($map[$long][$lat % $xMax] -eq '#') {
+            $map[$long][$lat % $xMax] = 'X'
+            $hits++
+        } else {
+            $map[$long][$lat % $xMax] = 'O'
+        }
+
+        # Shift 3 over for the next line
+        $lat+=$right
+        # Write-Host("{2} | {1} | {0}" -f ($long+1), ($map[$long]-join ""), $hits)
+    }
+    return $hits
 }
 
-# Write-Host $hits
+$answer = (HappyTrails 1 1) * (HappyTrails 3 1) * (HappyTrails 5 1) * (HappyTrails 7 1) * (HappyTrails 1 2) 
+Write-Host $answer
